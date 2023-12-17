4772bb82a851cada5f725ccc51274415f014e58d
ShirshoDipto <shirshodipto@gmail.com>
2023-12-18 02:02:29 +0600

    Calc permutations of a string recursively

--- DSA/recursion problems/recursive-backtracking.js
+++ DSA/recursion problems/recursive-backtracking.js
@@ -12,8 +12,33 @@ function fibonacci(n, tabs = "  ") {
   }
 }
 
-console.log(fibonacci(1));
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(4));
-console.log(fibonacci(5));
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(4));
+// console.log(fibonacci(5));
+
+// --------------------- #2 Permutation ----------------------
+function permutationHelper(remaining, soFar, allPerms, tabs = "  ") {
+  // console.log(`${tabs}permutationsHelper(${remaining}, ${soFar})`);
+  if (remaining === "") {
+    allPerms.add(soFar);
+  } else {
+    for (let i = 0; i < remaining.length; i++) {
+      const nextChar = remaining[i];
+      const newRemaining = remaining.slice(0, i) + remaining.slice(i + 1);
+      permutationHelper(newRemaining, soFar + nextChar, allPerms, tabs + "  ");
+    }
+  }
+}
+
+function permutation(str) {
+  const allPermutaations = new Set();
+  permutationHelper(str, "", allPermutaations);
+  return allPermutaations;
+}
+
+console.log(permutation("cat"));
+console.log(permutation("c"));
+console.log(permutation("tail"));
+console.log(permutation("HHT"));
