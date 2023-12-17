a9443f28e6cfbbec432dd04025626fc680e34a99
ShirshoDipto <shirshodipto@gmail.com>
2023-12-18 01:58:02 +0600

    Calc factorials of a number recursively

--- DSA/recursion problems/recursion.js
+++ DSA/recursion problems/recursion.js
@@ -5,6 +5,16 @@ function power(a, b) {
   return a * power(a, b - 1);
 }
 
-console.log(power(3, 0));
-console.log(power(0, 3));
-console.log(power(0, 0));
+// console.log(power(3, 0));
+// console.log(power(0, 3));
+// console.log(power(0, 0));
+
+// ---------------------- #2 Factorial-----------------------------
+function factorial(a) {
+  if (a === 0) return 1;
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(0));
+console.log(factorial(2));
+console.log(factorial(5));
